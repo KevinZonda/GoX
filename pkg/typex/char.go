b4af71074fb4fd4b9bb8e8d901ffab343b4336e1
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-10-29 23:37:16 +0100

    feat: impl. char

---
+++ pkg/typex/char.go
@@ -0,0 +1,44 @@
+package typex
+
+import "strings"
+
+type char rune
+
+func ToCharArray(s string) []char {
+	return []char(s)
+}
+
+func Length(s string) int {
+	return len(ToCharArray(s))
+}
+
+func ToString(cs []char) string {
+	return string(cs)
+}
+
+func (c char) ToString() string {
+	return string(c)
+}
+
+func (c char) ToRune() rune {
+	return rune(c)
+}
+
+func (c char) Append(s string) string {
+	var sb strings.Builder
+	sb.WriteRune(rune(c))
+	sb.WriteString(s)
+	return sb.String()
+}
+
+func (c char) Preappend(s string) string {
+	var sb strings.Builder
+	sb.WriteString(s)
+	sb.WriteRune(rune(c))
+	return sb.String()
+}
+
+func ToRunes(cs []char) []rune {
+	var m any = cs
+	return m.([]rune)
+}

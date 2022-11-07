f00de42db870eb8a5b846958421bda9351ddfd4a
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2022-11-07 02:22:22 +0000

    feat: console

---
+++ pkg/console/console.go
@@ -0,0 +1,22 @@
+package console
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var scanner = bufio.NewReader(os.Stdin)
+
+func ReadLine() (string, error) {
+	return scanner.ReadString('\n')
+}
+
+func WriteLine(format string, params ...any) {
+	fmt.Printf(format, params...)
+	fmt.Println("")
+}
+
+func Write(format string, params ...any) {
+	fmt.Printf(format, params...)
+}

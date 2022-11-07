3f28222ea4da2db7c32e14e02cb7a9caa994c4b5
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2022-11-07 02:34:02 +0000

    feat: process

---
+++ pkg/process/run.go
@@ -0,0 +1,45 @@
+package process
+
+import (
+	"os"
+	"os/exec"
+)
+
+type Runable struct {
+	Name string
+	Args []string
+	Wait bool
+}
+
+func (r Runable) run() (cmd *exec.Cmd, err error) {
+	if r.Wait {
+		return RunWithWait(r.Name, r.Args...)
+	}
+	return RunWithoutWait(r.Name, r.Args...)
+}
+
+func RunWithWait(name string, args ...string) (cmd *exec.Cmd, err error) {
+	cmd = exec.Command(name, args...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+
+	err = cmd.Start()
+	if err != nil {
+		return
+	}
+	err = cmd.Wait()
+	return
+}
+
+func RunWithoutWait(name string, args ...string) (cmd *exec.Cmd, err error) {
+	cmd = exec.Command(name, args...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+
+	err = cmd.Start()
+	return
+}

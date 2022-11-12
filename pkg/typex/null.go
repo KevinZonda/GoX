f022535ba37dcf0369392deead3a93cd234c225a
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-11-12 21:02:31 +0000

    nullable

--- pkg/typex/null.go
+++ pkg/typex/null.go
@@ -4,3 +4,16 @@ type Nullable[T any] struct {
 	IsNull bool
 	Value  T
 }
+
+func NewNotNull[T any](value T) Nullable[T] {
+	return Nullable[T]{
+		Value:  value,
+		IsNull: false,
+	}
+}
+
+func NewNull[T any]() Nullable[T] {
+	return Nullable[T]{
+		IsNull: true,
+	}
+}

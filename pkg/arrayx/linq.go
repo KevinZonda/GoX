2edc73950663c530b80794bc81fa0bd28059ecbd
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-11-19 16:14:14 +0000

    feat: support Map

--- pkg/arrayx/linq.go
+++ pkg/arrayx/linq.go
@@ -6,3 +6,11 @@ func Last[T any](l []T) T {
 	}
 	return l[len(l)-1]
 }
+
+func Map[T any, K any](from []T, tx func(T) K) []K {
+	var v []K
+	for _, t := range from {
+		v = append(v, tx(t))
+	}
+	return v
+}

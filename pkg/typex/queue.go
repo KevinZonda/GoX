03cd5948cee962eb8ec5a66c3aabbf76cb1d2b45
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-11-19 16:12:15 +0000

    feat: queue

---
+++ pkg/typex/queue.go
@@ -0,0 +1,29 @@
+package typex
+
+type Queue[T any] struct {
+	data []T
+}
+
+func (q *Queue[T]) Enqueue(data T) {
+	q.data = append(q.data, data)
+}
+
+func (q *Queue[T]) Dequeue() (data T, ok bool) {
+	data, ok = q.TakeFirst()
+	if ok {
+		q.data = q.data[1:]
+	}
+	return
+}
+
+func (q *Queue[T]) TakeFirst() (data T, ok bool) {
+	if len(q.data) == 0 {
+		return nil, false
+	}
+	data = q.data[0]
+	return data, true
+}
+
+func (q *Queue[T]) ToSlice() []T {
+	return q.data
+}

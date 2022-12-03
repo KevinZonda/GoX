084dc24f4ff8560990b83ba30b80fb6fbdf02d30
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-12-03 01:51:29 +0000

    feat: nullable

--- pkg/typex/null.go
+++ pkg/typex/null.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 )
 
-type Nullable[T any] interface {
+type INullable[T any] interface {
 	IsNull() bool
 	Value() (T, error)
 	NotNull()
@@ -14,12 +14,12 @@ type Nullable[T any] interface {
 	UnNull(value T)
 }
 
-type nullable[T any] struct {
+type Nullable[T any] struct {
 	isNull bool
 	value  T
 }
 
-func (n *nullable[T]) Value() (val T, err error) {
+func (n *Nullable[T]) Value() (val T, err error) {
 	if n.IsNull() {
 		err = errors.New("access null value")
 		return
@@ -28,36 +28,36 @@ func (n *nullable[T]) Value() (val T, err error) {
 	return
 }
 
-func (n *nullable[T]) IsNull() bool {
+func (n *Nullable[T]) IsNull() bool {
 	if n == nil {
 		return true
 	}
 	return n.isNull
 }
 
-func (n *nullable[T]) NotNull() {
+func (n *Nullable[T]) NotNull() {
 	if n.IsNull() {
 		panic(fmt.Sprintf("Nullable %+v is null however we confirm it should be notnull!", n))
 	}
 }
 
-func (n *nullable[T]) Null() {
+func (n *Nullable[T]) Null() {
 	n.isNull = true
 }
 
-func (n *nullable[T]) UnNull(value T) {
+func (n *Nullable[T]) UnNull(value T) {
 	n.isNull = false
 	n.value = value
 }
 
-func (n *nullable[T]) MarshalJSON() ([]byte, error) {
+func (n *Nullable[T]) MarshalJSON() ([]byte, error) {
 	if n.isNull {
 		return json.Marshal(nil)
 	}
 	return json.Marshal(n.value)
 }
 
-func (n *nullable[T]) UnmarshalJSON(b []byte) error {
+func (n *Nullable[T]) UnmarshalJSON(b []byte) error {
 	var m *T = nil
 	err := json.Unmarshal(b, &m)
 	if err != nil {
@@ -70,15 +70,15 @@ func (n *nullable[T]) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-func NewNotNull[T any](value T) Nullable[T] {
-	return &nullable[T]{
+func NewNotNull[T any](value T) *Nullable[T] {
+	return &Nullable[T]{
 		value:  value,
 		isNull: false,
 	}
 }
 
-func NewNull[T any]() Nullable[T] {
-	return &nullable[T]{
+func NewNull[T any]() *Nullable[T] {
+	return &Nullable[T]{
 		isNull: true,
 	}
 }

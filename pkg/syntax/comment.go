a949b7f2ada877282deef664cb1ccb2623acf289
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2022-12-16 16:46:39 +0000

    feat: more syntax

--- pkg/syntax/comment.go
+++ pkg/syntax/comment.go
@@ -5,3 +5,15 @@ package syntax
 func Comment(_ any) {
 
 }
+
+// Example is a comment in a source file.
+// Allows comment with syntax colouring
+func Example(_ any) {
+
+}
+
+// DoNot is a comment in a source file.
+// Allows comment with syntax colouring
+func DoNot(_ any) {
+
+}

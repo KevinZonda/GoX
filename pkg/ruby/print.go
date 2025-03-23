97f5a83bd9192ca5107cef33cfea5992addd6f96
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2025-03-23 01:10:25 +0000

    fix

---
+++ pkg/ruby/print.go
@@ -0,0 +1,35 @@
+package ruby
+
+import "fmt"
+
+func Println(v string) {
+	fmt.Println(v)
+}
+
+func Print(v string) {
+	fmt.Print(v)
+}
+
+func PrintlnInt(v int) {
+	fmt.Println(v)
+}
+
+func PrintInt(v int) {
+	fmt.Print(v)
+}
+
+func PrintlnFP64(v float64) {
+	fmt.Println(v)
+}
+
+func PrintFP64(v float64) {
+	fmt.Print(v)
+}
+
+func PrintlnFP32(v float32) {
+	fmt.Println(v)
+}
+
+func PrintFP32(v float32) {
+	fmt.Print(v)
+}
